91641332c9dae12589a4b096a4b985950397b055
gqfjob <gqfjob@gmail.com>
2014-08-10 22:59:26 +0800

    调整列表页样式

--- application/views/default/mate/list.php
+++ application/views/default/mate/list.php
@@ -1,23 +1,37 @@
 <style>
-.oneMaterail{
-}
 
 </style>
 <div id="container">
+<div class="crumb mb10 mt20">
+	<div class="fl ico crumb-ico mr5"></div>当前位置：
+        
+        <a href="###" target="_self" title="分类" hidefocus="true">分类列表</a>
+		&nbsp;&gt;&nbsp;
+        <a href="###" title="" hidefocus="true">文档</a>
+	</div>
 	<div class="oneMaterail">
 		<div class="mbasic">
 			<div class="mlogo">
 				<img src="/uploads/thumb/201408/1923edca95a28e3d808bd46d5899d255.png"/>
 			</div>
 			<div class="mdetail">
-				<p class="mtitle"></p>
-				<ul class="minfo">
-					<li></li>
-					<li></li>
-					<li></li>
+				<p class="mtitle"><a href="">缤纷世界，我的金彩</a></p>
+				<ul class="mtime">
+					<li style="width:150px">版本数：3</li>
+					<li>更新时间：2014-10-25 12:30</li>
 				</ul>
+				<div class="cl"></div>
+				<ul class="minfo">
+					<li style="width:150px">素材类型：文档</li>
+					<li style="width:250px">上传时间：2014-10-25 12:30</li>
+					<li style="width:300px;overflow:hidden">当前版本：V1.0开发预览版</li>
+				</ul>
+				<div class="cl"></div>
+				<div class="mcontent">
+					提供23张使用说明素材相关设计图片、背景、模板、素材下载。... 相关“使用说明素材” 23 张图片 相关素材使用说明素材 蚊香使用说明示意图消防栓使用说明消防栓使
+				</div>
 			</div>
+			<div class="cl"></div>
 		</div>
-		<div class="mcontent"></div>
 	</div>
 </div>
