77404f7fc578520b7b2053d185266457bbd27665
gqfjob <gqfjob@gmail.com>
2014-08-11 22:03:24 +0800

    新增素材详情和列表页面样式

--- application/views/default/mate/detail.php
+++ application/views/default/mate/detail.php
@@ -1,3 +1,140 @@
-<div id="container">
-详情页
+<style>
+.mdetail,.mcontent{
+	width:530px;
+}
+.mtype{
+	display:inline-block;
+	background:red;
+	width:50px;
+	height:30px;
+	line-height:30px;
+	padding:0px 0px 0px 10px;
+	margin-right:5px;
+	color:#fff;
+}
+.mtype a{
+	color:#fff;
+	background:red;
+}
+.mvdetail{
+	text-indent: 2em;
+	min-height:100px;
+	padding-bottom:20px;
+	line-height:24px;
+	padding-left:20px;
+	padding-right:20px;
+}
+
+.works-manage-box {
+	height: 55px;
+	background-color: #3f5260;
+}
+.works-manage-download {
+	width: 124px;
+	height: 55px;
+	line-height: 55px;
+	text-align: center;
+	background-color: #d01876;
+	overflow: hidden;
+	cursor: pointer;
+}
+.works-manage-download em {
+	display: inline-block;
+	font-style: normal;
+	font-size: 16px;
+	font-family: microsoft yahei;
+	color: #fff;
+	vertical-align: middle;
+}
+.mr5 {
+	margin-right: 5px;
+}
+.boxHd {
+	line-height: 18px;
+}
+.roundFont{
+	bord-moz-border-radius: 15px;
+	-webkit-border-radius: 15px;
+	border-radius:15px;
+	border:1px solid #2EC4F9;
+	padding:1px 5px;
+	color:#2EC4F9;
+	margin-right:5px;
+}
+</style>
+<div id="wcontainer">
+	<div class="crumb mb20 mt20">
+		<div class="fl ico crumb-ico mr5"></div>当前位置：
+	    <a href="javascript:void(0);" title="" hidefocus="true">素材详情</a>&nbsp;&gt;&nbsp;<a href="javascript:void(0);" title="" hidefocus="true">缤纷世界，我的金彩</a>
+	</div>
+	<div id="wPageLeft">
+		<!-- 素材基本信息介绍 -->
+		<div class="oneMaterail">
+			<div class="mbasic">
+				<div class="mlogo">
+					<img src="/uploads/thumb/201408/1923edca95a28e3d808bd46d5899d255.png"/>
+				</div>
+				<div class="mdetail">
+					<p class="mtitle"><span class="mtype"><a href="javascript:void(0);" style="color:#fff">图片</a></span><a href="">缤纷世界，我的金彩</a></p>
+					<ul class="mtime">
+						<li style="width:220px">上传时间：2014-10-25 12:30</li>
+						<li style="width:90px">版本数：3</li>
+					</ul>
+					<div class="cl"></div>
+					<ul class="minfo">
+						<li style="width:220px">更新时间：2014-10-25 12:30</li>
+						<li style="width:300px;overflow:hidden">当前版本：V1.0开发预览版</li>
+					</ul>
+					<div class="cl"></div>
+					<div class="mcontent">
+						提供23张使用说明素材相关设计图片、背景、模板、素材下载。... 相关“使用说明素材” 23 张图片 相关素材使用说明素材 蚊香使用说明示意图消防栓使用说明消防栓使
+					</div>
+				</div>
+				<div class="cl"></div>
+			</div>
+		</div>
+		<!-- 详情介绍 -->
+		<div class="wBoxLeft mt20">
+			<div>
+            	<div class="fl ico original-ico" style="margin:6px">&nbsp;</div>
+            	<h2 class="fl font-yahei b16">素材说明</h2>
+            	<div class="cl"></div>
+            </div>
+            <div class="mvdetail font-yahei font14">
+            
+            </div>
+		</div>
+		<!-- 素材附件-->
+		<div class="wBoxLeft mt20">
+			<div>
+            	<div class="fl ico media-ico" style="margin:8px">&nbsp;</div>
+            	<h2 class="fl font-yahei b16">素材附件</h2>
+            	<div class="cl"></div>
+            </div>
+            <div class="mvdetail font-yahei font14">
+            
+            </div>
+		</div>
+		<!-- 打包下载 -->
+		<div class="works-manage-box mt20">
+            <a href="###" title="点击进入下载" class="fr hover-none works-manage-download" hidefocus="true"><em class="mr5">打包下载</em><span class="download-ico2 ico inline-block vertical-middle"></span></a>
+        </div>
+	</div>
+	<div id="wPageRight">
+		<div class="wBoxRight">
+			<div class="boxHd">
+				<div class="fl down-rank-ico ico" style="margin:5px 5px 0px 10px"></div><div class="fl b16 font-yahei mt5">其他版本下载</div><div class="fr mt5 font12 roundFont">版本数:5</div>
+				<div class="cl"></div>
+			</div>
+			<div style="height: 200px"></div>
+		</div>
+		<div class="wBoxRight mt20">
+			<div class="boxHd">
+				<div class="fl keywords-ico ico" style="margin:5px 5px 0px 10px"></div><h2 class="fl b16 font-yahei">同类型素材</h2>
+				<div class="cl"></div>
+			</div>
+			<div style="height: 200px"></div>
+		</div>
+	</div>
+	<div class="cl"></div>
 </div>
