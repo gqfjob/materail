105b0a897b11bb56403a1589b65425e958f04083
gqfjob <gqfjob@gmail.com>
2015-08-11 17:33:35 +0800

    test

--- application/controllers/material.php
+++ application/controllers/material.php
@@ -1037,7 +1037,7 @@ class Material extends CI_Controller {
 			debug_log($key);
 			foreach ($res as $r)
 			{
-				if(strpos($r['mname'],$key)){
+				if(stripos($r['mname'],$key) > 0){
 					$r['mname'] =str_ireplace($key, '<span class="hightlight">'.$key.'</span>', $r['mname']);
 					debug_log($r['mname']);
 				}
