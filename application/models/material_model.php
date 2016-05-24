b8b79d94e02ea9720a0f4cf9793f9c4157c61cb4
gqfjob <gqfjob@gmail.com>
2016-05-24 16:05:17 +0800

    test2

--- application/models/material_model.php
+++ application/models/material_model.php
@@ -1549,7 +1549,7 @@ class Material_Model extends CI_Model
 		$sql = "select distinct v.id as vid, m.*,v.*,c.cname,c.clogo  from material_info as m right join material_version as v  on m.id = v.mid ";
 		$sql .= " left join material_cate as c on m.cid = c.id ";
 		$sql .= " LEFT JOIN material_attatch AS a ON m.id = a.mid ";
-		$sql .= " where (m.mname like '%".$key."%' or v.nohtml like '%".$key."%'  or a.sname like '%".$key."%') and m.state=1 ";
+		$sql .= " where (m.mname like '%".trim($key)."%' or v.nohtml like '%".trim($key)."%'  or a.sname like '%".trim($key)."%') and m.state=1 ";
 		if(($cur != 'all') && is_numeric($cur)){
 			if($cur > 0){
 				$sql .= " and m.cid = ".$cur;
@@ -1575,7 +1575,7 @@ class Material_Model extends CI_Model
 		$sql = "select distinct v.id as vid, m.*,v.*,c.cname,c.clogo  from material_info as m right join material_version as v  on m.id = v.mid ";
 		$sql .= " left join material_cate as c on m.cid = c.id ";
 		$sql .= " LEFT JOIN material_attatch AS a ON m.id = a.mid ";
-		$sql .= " where (m.mname like '%".$key."%' or v.nohtml like '%".$key."%'  or a.sname like '%".$key."%') and m.state=1 ";
+		$sql .= " where (m.mname like '%".trim($key)."%' or v.nohtml like '%".trim($key)."%'  or a.sname like '%".trim($key)."%') and m.state=1 ";
 		if(($cur != 'all') && is_numeric($cur)){
 			if($cur > 0){
 				$sql .= " and m.cid = ".$cur;
